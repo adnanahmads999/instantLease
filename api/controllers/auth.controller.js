a291e3693b12286b10eabcc6c316134d49069afd
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-04 20:55:44 +0530

    add error handle functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,14 +2,21 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
 
-export const signup = async (req, res) => {
+// sign up route 
+export const signup = async (req, res,next) => {
+
+        // create a user model
     const { username, email, password } = req.body;
+
+    //give authentication of password using hashed password 
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
+
+      // give try catch for if in background erro or duplicate occurence then give a error message
     try {
         await newUser.save();
         res.status(201).json('User created successfully!');
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
