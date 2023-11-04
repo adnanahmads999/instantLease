07130b3d61d68b84f32de6af93401309431f8115
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-04 20:24:57 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// mongoose connection settings
+mongoose.connect(process.env.MONGO_URI,{
+    dbName: "InstantLease",
+})
+.then(()=>console.log("mongodb connected"))
+.catch((e)=>console.log(e));
 
 const app = express();
 
-app.listen(8080,()=>{
-    console.log("Server listening on port 3000!");
-});
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on http://localhost:${process.env.PORT}`);
+})
